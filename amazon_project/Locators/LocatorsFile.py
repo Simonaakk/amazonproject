961326ab72a715c2108a51202a71f25816603fde
Simonaakk <simn7@rambler.ru>
2022-05-23 20:34:45 +0400

    Negative SignIn

--- amazon_project/Locators/LocatorsFile.py
+++ amazon_project/Locators/LocatorsFile.py
@@ -22,3 +22,23 @@ userHomePageCartButtonLocator = (By.ID, "nav-cart")
 
 cartPageCountLocator = (By.ID, "nav-cart-count")
 cartPageDeleteButton = (By.XPATH, "(//input[@value='Delete'])[1]")
+
+#Main Page Home Button
+mainPageHomeButtonLocator = (By.ID, "nav-logo-sprites")
+
+#Main Page Search Field
+mainPageSearchFieldLocator = (By.ID, "twotabsearchtextbox")
+mainPageSearchButtonLocator = (By.ID, "nav-search-submit-button")
+
+#Searched Page
+searchedFirstItemLocator = (By.XPATH, "//img[@class='s-image']")
+
+#First Item Page
+firstItemAddToCartButtonLocator = (By.ID, "add-to-cart-button")
+
+#Login Page Wrong Login Error
+wrongLoginErrorTextLocator = (By.CLASS_NAME, "a-list-item")
+
+#SignOut
+accountButtonLocator = (By.ID, "nav-link-accountList")
+signOutButtonLocator = (By.ID, "nav-item-signout")
